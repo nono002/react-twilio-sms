33f44c60094b5fb2bdfa6987ee094cd45308984b
Serg Studzinskiy <61849458+nono002@users.noreply.github.com>
2021-02-18 16:00:00 +0200

    mistakes errors for sms

--- src/App.js
+++ src/App.js
@@ -4,12 +4,38 @@ import { Container, Form, Button } from "react-bootstrap";
 function App() {
   const [number, setNumber] = useState("");
   const [body, setBody] = useState("");
+
+  const onSubmit = async (e) => {
+    await e.preventDefault();
+
+    const res = await fetch("/api/sendMessage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ to: number, body: body }),
+    });
+
+    const data = await res.json();
+
+    if (data.success) {
+      await setNumber("");
+      await setBody("");
+    } else {
+      await setNumber("An Error has occurred.");
+      await setBody("An Error has occurred.");
+    }
+  };
   
   return (
     <div className="App">
 		<Container>
 			<h2>Send SMS</h2>
-			<Form>
+			<Form onSubmit={onSubmit}>
+				<Form.Group>
+					<Form.Label htmlFor="to">To</Form.Label>
+					<Form.Control value={number} onChange={(e) => setNumber(e.target.value)} />
+				</Form.Group>
 				<Form.Group>
 					<Form.Label htmlFor="message">Body</Form.Label>
 						<Form.Control
@@ -18,10 +44,6 @@ function App() {
 							value={body}
 							onChange={(e) => setBody(e.target.value)}
 						/>
-				</Form.Group>
-				<Form.Group>
-					<Form.Label htmlFor="message">Body</Form.Label>
-					<Form.Control as="textarea" rows="3" />
 				</Form.Group>
 				<Button variant="primary" type="submit">
 					Send
